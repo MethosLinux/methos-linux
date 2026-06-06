405c8d2d35ddd2ca5169328d9a5ec6f39ee55fbf
Methos Linux Admin <admin@methos-linux.org>
2026-06-07 01:05:02 +0300

    fix: add efiboot/loader/entries/archiso.conf, fix bootmodes names

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 # Methos Linux - archiso profile definition
-# Compatible with mkarchiso v80+
-# NOTE: bootmodes (no underscore) is the correct variable name in modern mkarchiso
 
 profile_name="methos-linux"
 iso_label="METHOS_$(date +%Y%m)"
@@ -11,7 +9,7 @@ iso_application="Methos Linux Live/Installation System"
 iso_volume_id="METHOS_LINUX"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.grub.mbr' 'uefi-x64.systemd-boot.esp')
+bootmodes=('bios.grub' 'uefi.systemd-boot')
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'zstd')
 pacman_conf="pacman.conf"
