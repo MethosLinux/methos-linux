1301f694257ae7f92c723600d269e033b8be03a3
Methos Linux Admin <admin@methos-linux.org>
2026-06-07 01:09:03 +0300

    fix: add loader.conf, use uefi-x64.systemd-boot.esp only

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC2034
-# Methos Linux - archiso profile definition
+# Minimal profile matching Arch releng exactly
 
 profile_name="methos-linux"
 iso_label="METHOS_$(date +%Y%m)"
@@ -9,7 +8,7 @@ iso_application="Methos Linux Live/Installation System"
 iso_volume_id="METHOS_LINUX"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.grub' 'uefi.systemd-boot')
+bootmodes=('uefi-x64.systemd-boot.esp')
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'zstd')
 pacman_conf="pacman.conf"
