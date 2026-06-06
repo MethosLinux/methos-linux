749a7579d2118fa0d83e50e64a0c46af51870e7b
Methos Linux Admin <admin@methos-linux.org>
2026-06-07 00:41:45 +0300

    fix: profiledef.sh match latest mkarchiso format with buildmodes and install_dir

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -1,45 +1,20 @@
 #!/usr/bin/env bash
 # Methos Linux - archiso profile definition
-# Based on official Arch Linux releng profile
-# Reference: https://gitlab.archlinux.org/archlinux/archiso/-/blob/master/configs/releng/profiledef.sh
+# This file is sourced by mkarchiso, not executed
+# Variables are read by mkarchiso internally
 
 profile_name="methos-linux"
+
 iso_label="METHOS_$(date +%Y%m)"
 iso_publisher="Methos Linux Project <https://github.com/methos-linux>"
 iso_application="Methos Linux Live/Installation System"
 iso_volume_id="METHOS_LINUX"
-iso_version="$(cat VERSION 2>/dev/null || echo '0.1.0-alpha')-$(date +%Y.%m.%d)"
-iso_filename="methos-linux-${iso_version}-x86_64.iso"
+iso_version="$(date +%Y.%m.%d)"
+iso_filename="methos-linux-x86_64.iso"
 
-# Boot modes: GRUB handles both BIOS + UEFI
-# uefi_arch is readonly - set automatically by mkarchiso
-boot_modes=(
-    "bios+uefi-x64.grub.esp"
-)
+install_dir="arch"
+buildmodes=('iso')
+boot_modes=('bios.grub.mbr' 'uefi-x64.systemd-boot.esp')
 
-# Filesystem
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=(
-    "-comp" "zstd"
-    "-Xcompression-level" "15"
-    "-no-duplicates"
-)
-
-# Permissions
-file_permissions=(
-    "/etc/shadow:0:0:0400"
-    "/etc/gshadow:0:0:0400"
-    "/etc/sudoers:0:0:0440"
-    "/etc/sudoers.d/10-methos:0:0:0440"
-    "/etc/calamares:0:0:755"
-    "/etc/calamares/scripts/merge_profiles.sh:0:0:755"
-)
-
-# Symlinks
-file_symlinks=(
-    "/usr/bin/sh:bash"
-    "/usr/bin/editor:nvim"
-)
-
-# GRUB config path (relative to this profile directory)
-uefi_grub_cfg="grub/cfg/grub.cfg"
+airootfs_image_tool_options=('-comp' 'zstd')
