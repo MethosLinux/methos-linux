a47124a83a05c7c4209c7e8035e42bbc25a6905d
Methos Linux Admin <admin@methos-linux.org>
2026-06-07 05:32:38 +0300

    fix: use uefi-x64.grub.esp instead of systemd-boot to fix initrd error

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-# Methos Linux - archiso profile
-# Compatible with archiso v72 (GitHub Actions version)
+# Methos Linux - archiso profile definition
 
 profile_name="methos-linux"
 iso_label="METHOS_$(date +%Y%m)"
@@ -9,7 +8,7 @@ iso_application="Methos Linux Live/Installation System"
 iso_volume_id="METHOS_LINUX"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('uefi-x64.systemd-boot.esp')
+bootmodes=('uefi-x64.grub.esp')
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'zstd')
 pacman_conf="pacman.conf"
